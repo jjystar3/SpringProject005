96847a4272df23f1de39e7114c596e3dc18d3595
JuYoung <jjystar3@gmail.com>
2024-09-23 16:30:08 +0900

    템플릿 레이아웃 준비

---
+++ src/main/java/com/example/demo/controller/LayoutController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/layout")
+public class LayoutController {
+	
+	@GetMapping("/exLayout01")
+	public void ex01() {
+		// 리턴타입이 void면 URL경로가 HTML파일의 경로가 된다
+	}
+
+}
