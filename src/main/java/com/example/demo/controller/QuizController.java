5d3efd544d5dfe506925345244b00b4276125783
JuYoung <jjystar3@gmail.com>
2024-09-23 16:10:41 +0900

    quiz 풀이

--- src/main/java/com/example/demo/controller/QuizController.java
+++ src/main/java/com/example/demo/controller/QuizController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,4 +41,30 @@ public class QuizController {
 		BookDTO bookDTO = new BookDTO("스프링부트웹프로젝트", "구멍가게코딩단", LocalDate.of(2022,12,25));
 		model.addAttribute("dto", bookDTO);
 	}
+
+    @GetMapping("/q05")
+    public void ex05(Model model){
+    	int[] intArr = {1,2,3,4,5,6,7,8,9,10};
+        model.addAttribute("arr", intArr);
+    }
+
+    @GetMapping({"/q06", "/q07"})
+    public void ex06(Model model){
+        List<PersonDTO> dtoList = new ArrayList<>();
+        dtoList.add(new PersonDTO("둘리",20,"인천 구월동"));
+        dtoList.add(new PersonDTO("또치",30,"서울 신림동"));
+        dtoList.add(new PersonDTO("도우너",40,"부산 문래동"));
+        model.addAttribute("list", dtoList);
+    }
+
+    @GetMapping({"/q08", "/q09"})
+    public void ex08(Model model){
+        List<PersonDTO> dtoList = new ArrayList<>();
+        dtoList.add(new PersonDTO("박하나",25,"인천 구월동"));
+        dtoList.add(new PersonDTO("홍재범",17,"서울 신림동"));
+        dtoList.add(new PersonDTO("문유리",31,"부산 문래동"));
+        dtoList.add(new PersonDTO("김재민",8,"인천 연수동"));
+        dtoList.add(new PersonDTO("장유라",33,"부산 문래동"));
+        model.addAttribute("list", dtoList);
+    }
 }
