1decffdb3c3621faa427cd3492bd07747b419b98
JuYoung <jjystar3@gmail.com>
2024-09-24 11:06:17 +0900

    레이아웃

--- src/main/java/com/example/demo/controller/LayoutController.java
+++ src/main/java/com/example/demo/controller/LayoutController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/layout")
 public class LayoutController {
-	
-	@GetMapping("/exLayout01")
-	public void ex01() {
+
+	@GetMapping({"/exLayout01","/exLayout02" , "/fragment02", "/fragment03", "/basic", "/exSidebar"})
+	public void ex1() {
 		// 리턴타입이 void면 URL경로가 HTML파일의 경로가 된다
 	}
-
 }
